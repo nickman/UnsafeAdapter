fc19704503286e086e45b39eb929115b28f685ae
Nicholas Whitehead <nwhitehead@heliosdev.org>
2014-08-04 15:56:00 -0400

    DevSync

--- ua-benchmarks/src/main/java/perf/com/heliosapm/trove/BenchmarkTroveLongLongMap.java
+++ ua-benchmarks/src/main/java/perf/com/heliosapm/trove/BenchmarkTroveLongLongMap.java
@@ -24,31 +24,23 @@
  */
 package perf.com.heliosapm.trove;
 
-import java.util.Arrays;
-import java.util.Collection;
-
 import gnu.trove.map.hash.TLongLongHashMap;
+import gnu.trove.map.hash.UnsafeTLongLongHashMap;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.generators.core.ClassInfo;
-import org.openjdk.jmh.generators.core.GeneratorSource;
-import org.openjdk.jmh.runner.Runner;
-import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 /**
- * <p>Title: BecnhmarkTroveLongLongMap</p>
+ * <p>Title: BenchmarkTroveLongLongMap</p>
  * <p>Description: JMH Benchmark for the trove {@link TLongLongHashMap}</p> 
  * <p>Company: Helios Development Group LLC</p>
  * @author Whitehead (nwhitehead AT heliosdev DOT org)
- * <p><code>perf.com.heliosapm.trove.BecnhmarkTroveLongLongMap</code></p>
+ * <p><code>perf.com.heliosapm.trove.BenchmarkTroveLongLongMap</code></p>
  */
 @State(Scope.Thread)
-public class BecnhmarkTroveLongLongMap {
-	protected int initialSize = 1024;
+public class BenchmarkTroveLongLongMap {
+	protected int initialSize = 4214397;
 	protected int maxSize = 4214397;
 	protected float loadFactor = 0.75f;
 	protected long NULL = -1L;
@@ -56,7 +48,7 @@ public class BecnhmarkTroveLongLongMap {
 	
 
 	@Benchmark	
-	public void testMap() {
+	public int testTroveLongLongMap() {
 		final TLongLongHashMap map = new TLongLongHashMap(initialSize, loadFactor, NULL, NULL);   
 		int errors = 0;
     	for(int i = 0; i < maxSize; i++) {
@@ -68,25 +60,47 @@ public class BecnhmarkTroveLongLongMap {
     			errors++;
     		}
     	}
-    	
-    	log("Done: %s, errors: %s", map.size(), errors);
+    	return errors;
+    	//log("Done: %s, errors: %s", map.size(), errors);
+	}
+	
+	@Benchmark	
+	public int testUnsafeTroveLongLongMap() {
+		final UnsafeTLongLongHashMap map = new UnsafeTLongLongHashMap(initialSize, loadFactor, NULL, NULL);   
+		try {
+			int errors = 0;
+	    	for(int i = 0; i < maxSize; i++) {
+	    		map.put(i, i);
+	    	}
+	    	for(int i = 0; i < maxSize; i++) {
+	    		long x = map.get(i);
+	    		if(x!=i) {
+	    			errors++;
+	    		}
+	    	}
+	    	return errors;
+		} finally {
+			map.free();
+		}
 		
+    	//log("Done: %s, errors: %s", map.size(), errors);
 	}
 	
 	
-	public static void main(String[] args) throws RunnerException {
-		log("Starting Benchmarks on BecnhmarkTroveLongLongMap");
-        Options opt = new OptionsBuilder()
-        
-	        .include(".*")
-	        .forks(1)
-	        .build();
-        log("Includes: [%s]", opt.getIncludes());
-        new Runner(opt).run();
-    }	
+	
+//	public static void main(String[] args) throws RunnerException {
+//		log("Starting Benchmarks on BenchmarkTroveLongLongMap");
+//        Options opt = new OptionsBuilder()
+//        
+//	        .include(".*")
+//	        .forks(1)
+//	        .build();
+//        log("Includes: [%s]", opt.getIncludes());
+//        new Runner(opt).run();
+//    }	
 	
     public static void log(Object fmt, Object...args) {
-    	System.out.println(String.format(fmt.toString(), args));
+//    	System.out.println(String.format(fmt.toString(), args));
     }
 	
 }
